9fe2ad1214748ce7c36d8a987b1b1235dd86c1d4
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-12 18:52:07 -0300

    Comentários em php

---
+++ Cap2-Conceitos-basicos/4_Comentarios/index.php
@@ -0,0 +1,9 @@
+<?php   
+
+    # Comentarios servem para dar informações e direções importantes para um terceiro; Inicicamos com // ou #
+    # Todo comentário é ignorado na execução; Comentários multi linhas são feitos com /* comentário */
+
+    //Este é um comentário
+    echo("Teste comentário");
+
+?>
