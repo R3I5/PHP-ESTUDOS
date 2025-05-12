6ebce520225c161d9be15a50169b9dbcfd067cd0
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-12 17:46:43 -0300

    Case sensitive

--- Cap2-Conceitos-basicos/CaseSensivity/index.php
+++ Cap2-Conceitos-basicos/CaseSensivity/index.php
@@ -2,6 +2,16 @@
 
 # Sensibilidade entre casas maiúsculas e minúsculas; Para php não existe essa diferença
 
+echo "teste <br> ";
+eChO "testando 2 <br>";
+EcHo "testando 3 <br>";
+
+# comando <br> pula linhas
+
+$nome = "João";
+echo $nome
+
+#variáveis são case sensitive
 
 
 
