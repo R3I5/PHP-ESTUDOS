31197f70b0e1fe14e30a3d615c1638ebe0d4482a
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-11 21:24:29 -0300

    Update Exercicio 1

--- Introducao/Ex1.php
+++ Introducao/Ex1.php
@@ -1,7 +1,8 @@
-// Exercicio 1 
-// Crie um arquivo PHP; Este arquivo deve ter uma instrução que imprime o seu nome; Execute o arquivo
-
 <?php 
+
+    // Exercicio 1  
+    // Crie um arquivo PHP; Este arquivo deve ter uma instrução que imprime o seu nome; Execute o arquivo
+
 
     echo('João Victor dos Reis');
 
