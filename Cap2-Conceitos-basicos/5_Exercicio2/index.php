cf4d44cbe48095ca9f019f6f5ab7d6400b266f12
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-12 18:55:33 -0300

    Exercicio Comentarios PHP

---
+++ Cap2-Conceitos-basicos/5_Exercicio2/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    #Crie um arquivo PHP; Neste arquivo descreva caracteristicas básicas de uma pessoa ou objeto utilizando comentários; Pelo menos três, em linhas separadas
+
+    //Nome:
+    echo("João <br>");
+
+    //Idade:
+    echo("20 anos <br>");
+
+    //Altura:
+    echo("1.70m")
+
+?>
