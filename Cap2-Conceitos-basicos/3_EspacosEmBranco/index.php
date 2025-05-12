b51e6fc6223d8f49e55d934233ad22bf58a02d71
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-12 18:45:25 -0300

    Update Files

---
+++ Cap2-Conceitos-basicos/3_EspacosEmBranco/index.php
@@ -0,0 +1,14 @@
+<?php   
+
+    # Para interpretação do código em PHP o espaço em branco é ignorado; Isso acontece pois o mesmo é removido antes da execução
+    # A quebra de linha também é ignorada; Apesar de não levarem a problemas, essa práticas são evitadas visando o clean code
+
+    echo                "testando espaço em branco <br>";
+    echo "testando espaço <br>";
+    echo 
+    "quebra de linha <br> ";
+    echo "quebra 
+    de linha ";
+
+
+?>
