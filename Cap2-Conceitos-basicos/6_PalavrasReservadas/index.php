637a857233900337ed663119e2aa7513b26299ce
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-12 19:01:54 -0300

    Palavras reservadas

---
+++ Cap2-Conceitos-basicos/6_PalavrasReservadas/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    # Algumas palavras são reservadas da linguagem e já tem suas funcionalidades definidas, então não podemos utilizar em nossos programas
+    # Caso fosse possível poderiamos acabar substituindo sua funcção
+    # Alguns exemplos são: echo, insteadof, else, interface, namespace, pow, print, private, protected, and, require, public, as, break entre outras
+
+
+
+    // function echo(){
+    //   return true; 
+    //    }
+
+
+    function verdade(){
+        return true;
+    }
+
+?>
