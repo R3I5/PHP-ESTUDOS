af66cad46d85884de994a31ad963c29a94c338ef
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-12 18:39:29 -0300

    Instruções de código

---
+++ Cap2-Conceitos-basicos/InstrucaoDeCodigo/index.php
@@ -0,0 +1,14 @@
+<?php   
+
+    # as instruções simples são separdas por ponto e virgula; instruções simples são instruções de uma linha;
+    # em instrução maiores, como if e for, a definição é dada por abertura e fechamento de chaves;
+
+    echo "Uma linha <br> ";
+    echo "Segunda linha <br>";
+
+    if(5>2){
+        echo("Dentro do if");
+    }
+
+
+?>
