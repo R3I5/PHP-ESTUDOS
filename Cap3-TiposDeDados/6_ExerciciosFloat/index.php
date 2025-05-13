18d4b35f457bc161b781604d3b0af660212fc021
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 11:31:46 -0300

    Exercicio Validando Float

---
+++ Cap3-TiposDeDados/6_ExerciciosFloat/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    # Crie um arquivo PHP; Imprima três floats; Utilize a funcão is_float() em um deles;
+
+    echo 1.2;
+    echo'<br>';
+    $a = 1.7;
+    echo($a);
+    echo'<br>';
+    $b = $a + 1.4;
+    if(is_float($b)){
+        echo($b);
+    }
+
+?>
