2da30192900922962e705a41c69302c98ee2a2d1
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 14:15:10 -0300

    Escopo de variaveis

---
+++ Cap4-Variaveis/6_Escopo/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    # Como em outras linguagens, em PHP tambem temos escopo de variaveis; 
+    # Local: variavel declarada em uma função
+    # Global: variavel declarada fora de funções
+    # Static: variável declarada dentro da função, porem o seu valor permanece salvo entre chamadas da função
+    # Parâmetros da função: variaveis passadas para uma função, podendo ser utilizada ao longo da mesma
+
+    
+
+
+?>
