a31f7e2654acaaa851f0baaef9ee0cd969514d1c
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 12:53:19 -0300

    Declarando variaveis

---
+++ Cap4-Variaveis/1_DeclarandoVariaveis/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    # São a forma que temos para declarar um valor e salva-lo na memoria; Uma variavel em PHP tem o $ na frente do seu nome; $nome = 'Joao'
+    # Podemos salvar qualquer tipo de dado; Podemos alterar o valor de uma variavel ao decorrer do programa; 
+    # Podemos imprimir o valor de uma variavel com echo
+
+    $teste = "Algum texto";
+    echo "$teste";
+    echo"<br>";
+    $numeros = "10";
+    echo"$numeros";
+
+?>
