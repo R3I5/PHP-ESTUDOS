a1601fce76f6b4603d791a9a75f3cbffc34f64cf
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 13:34:17 -0300

    Exercicio Variaveis

---
+++ Cap4-Variaveis/3_ExercicioOperacaoVariaveis/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    # Crie um arquivo PHP; Crie duas variaveis com numeros; Crie uma terceira variavel com a soma destes dois numeros;
+
+    $numero1 = 20;
+    $numero2 = 3;
+    $soma = $numero1 + $numero2;
+    echo"$soma";
+
+
+?>
