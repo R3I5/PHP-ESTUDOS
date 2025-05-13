30bc470182fc7856fe0296a8eaead5b10eab8160
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 11:40:09 -0300

    Checando strings

---
+++ Cap3-TiposDeDados/8_ChecandoStrings/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    # Podemos utilizar a função is_string() para verificar se um dado é string; A função recebe um valor como parâmetro;
+    # Novamente recebemos true or false; Precisamos utilizar uma estrutra if para validar o valor;
+
+
+    $nome = "João";
+    if(is_string(($nome))){
+        echo("$nome é uma string!");
+    }
+
+
+
+?>
