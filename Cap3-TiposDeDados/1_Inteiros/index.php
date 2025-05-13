32262f5d69c6eb857993c703f3647e5df44a84ce
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-12 21:23:55 -0300

    Inteiros

---
+++ Cap3-TiposDeDados/1_Inteiros/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+    # Os inteiros são os números inteiros da matemática; como: 1, 2, 3, 15; Incluí os numeros negativos; 
+    # Os números positivos não precisam de um sinal + na frente; Já os números negativos devem ser escritos assim: -12
+
+    
+
+?>
