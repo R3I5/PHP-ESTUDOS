a3915e3e8a45b454ac7fd3977032360080b70111
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 11:37:00 -0300

    Checando strings

---
+++ Cap3-TiposDeDados/7_Strings/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+    # Os textos são conhecidos como strings; Em PHP podemos escrever textos em aspas simples ou duplas; As aspas duplas interpretam variáveis;
+
+    echo "Testando texto com aspas dupals <br>";
+    echo 'Testando texto com aspas simples <br>';
+    echo "Ele disse: 'Olá!' <br>";
+    echo 'Ela disse: "Olá!" <br>';
+
+    $idade = 20;
+
+    echo "Ele tem $idade anos <br>";
+    echo 'Ele tem $idade anos';
+
+
+
+?>
