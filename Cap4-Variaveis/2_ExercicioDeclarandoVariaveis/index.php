7d7032dc393c7ad1be34f2aa61e964fc0c22855e
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 12:56:14 -0300

    Exercicio Declarando Variaveis

---
+++ Cap4-Variaveis/2_ExercicioDeclarandoVariaveis/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    # Crie um arquivo PHP;
+    # Crie três variaveis com tipos de dados diferentes;
+    # Imprima estas variaveis;
+
+    $nome = "João";
+    $idade = "20";
+    $altura = "1.70";
+
+    echo"$nome tem $idade anos e $altura cm";
+
+?>
