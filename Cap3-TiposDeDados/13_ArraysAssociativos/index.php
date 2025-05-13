e2381324227565415bbb5ad4e65abee24581c5c9
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-13 12:19:59 -0300

    Arrays associativos

---
+++ Cap3-TiposDeDados/13_ArraysAssociativos/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+    # O array associativo é basicamente um array, porém com chave e valor; A estrutura base é a mesma, mas vamos construir dessa maneira:
+    # $arr = ['Nome'=>'Matheus', idade => 29]
+    # Chave entre aspas, seta para apontar o indice e o valor
+
+    $arr = ['nome' => 'Shark', 'patas' => 4, 'cor' => 'Marrom'];
+    echo $arr['nome'];
+    echo "<br>";
+    print_r($arr);
+    echo "<br>";
+    echo $arr['patas'];
+
+
+
+?>
