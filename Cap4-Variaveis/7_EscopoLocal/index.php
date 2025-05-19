9041b29cb40cf014de291d5138bfe32c97905748
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-19 08:22:35 -0300

    Aula Escopo Local

---
+++ Cap4-Variaveis/7_EscopoLocal/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    # A variável local tem seu escopo definido dentro de uma função; Ela não é acessivel fora da mesma;
+    # O seu valor sempre é resetado quando a função é finalizada; Obs: Veremos funções em detalhes futuramente;
+
+    
+
+
+
+
+
+
+
+?>
