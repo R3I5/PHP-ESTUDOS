6c8872d805e9ead8b5d6ec51a2fe837e465b8d6d
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-20 13:34:15 -0300

    Escopo Local

--- Cap4-Variaveis/7_EscopoLocal/index.php
+++ Cap4-Variaveis/7_EscopoLocal/index.php
@@ -3,12 +3,25 @@
     # A variável local tem seu escopo definido dentro de uma função; Ela não é acessivel fora da mesma;
     # O seu valor sempre é resetado quando a função é finalizada; Obs: Veremos funções em detalhes futuramente;
 
-    
+    $x = 10;
+    echo "$x global <br>";
 
+    function teste() {
+        
+        $x = 5;
+        
+        echo "$x local <br>";
+    }
 
+    $x = 99;
 
+    function testando() {
 
-
-
+        $x = 12;
+        echo "$x local 2 <br>";
+    }
+    teste();
+    testando();
+    echo "$x global 2 <br>";
 
 ?>
