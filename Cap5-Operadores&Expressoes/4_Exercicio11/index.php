82d935918b7a0d315df47949401c7f52f5232ffd
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-05-26 11:42:35 -0300

    Operadores Exercicio 11

---
+++ Cap5-Operadores&Expressoes/4_Exercicio11/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    # Crie um arquivo PHP;
+    # Teste a expressão "5" * 12;
+    # Utilize a função gettype() com o resultado como parâmetro para checar o tipo resultante da operação;
+
+
+    $a = "5" * 12;
+    echo "$a";
+    echo "<br>";
+    $b = gettype($a);
+    echo "$b";
+
+
+
+
+
+?>
