274d8055b56a912596428c0d3d266ed3b6f93167
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-06-04 08:50:08 -0300

    Operador de módulo

---
+++ Cap5-Operadores&Expressoes/7_OperadorMódulo/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+    # Operador de Módulo
+    # O operador de módulo é inserido no código pelo simbolo de %
+    # Sua função é realizar uma divisão;
+    # Mas como resultado ele apresenta apenas o resto da mesma;
+
+    
+
+
+
+
+
+
+
+?>
