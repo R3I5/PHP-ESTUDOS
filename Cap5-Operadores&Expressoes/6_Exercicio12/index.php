8ae4c9fe6cf70765c45d7091b8472e590b9d8724
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-06-04 08:28:38 -0300

    Exercicio 12 Operadores

---
+++ Cap5-Operadores&Expressoes/6_Exercicio12/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+    # Criar um arquivo PHP 
+    # Crie uma operação com cada um dos operadores básicos
+    # Cada operação deve estar em uma variável diferente
+    # Imprima cada uma das etapas
+
+    $soma = 4 + 1;
+    $mult = 2 * 2;
+    $div = 6/2;
+    $sub = 5 - 3;
+    $exp = 1**1;
+
+    echo("$soma<br>");
+    echo("$mult<br>");
+    echo("$div<br>");
+    echo("$sub<br>");
+    echo("$exp<br>")
+    
+
+
+
+
+
+?>
