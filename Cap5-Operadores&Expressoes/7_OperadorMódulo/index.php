91a6979ad30964071eeeaed72dd0d450177beda0
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-06-27 12:33:54 -0300

    Operador de módulo

--- Cap5-Operadores&Expressoes/7_OperadorMódulo/index.php
+++ Cap5-Operadores&Expressoes/7_OperadorMódulo/index.php
@@ -5,7 +5,12 @@
     # Sua função é realizar uma divisão;
     # Mas como resultado ele apresenta apenas o resto da mesma;
 
-    
+    echo  12%2;
+    echo "<br>";
+    echo "Módulo <br>";
+    echo 12/2;
+    echo "<br>";
+    echo "divisão <br>"
 
 
 
