84e60c3ad2d59ff3e2241dd286030acef5e769ad
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-07-13 20:42:20 -0300

    Exercicio 13 Operadores completo

--- Cap5-Operadores&Expressoes/8_Exercicio13/index.php
+++ Cap5-Operadores&Expressoes/8_Exercicio13/index.php
@@ -4,12 +4,13 @@
     # Teste o operador de resto em duas diviões;
     # Uma não exata e outra exata;
 
+    $a = 14;
+    $b = 2;
+    $c = 3;
 
-
-
-
-
-
-
+    echo $a % $b; //0
+    echo "<br>";
+    echo $a % $c; //2
+    echo "<br>"
 
 ?>
