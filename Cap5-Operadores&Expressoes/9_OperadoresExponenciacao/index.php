fbc623c63b8cd986117ca9dba535dcd5ad5a3511
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-07-13 20:45:45 -0300

    Operador de Exponenciação

---
+++ Cap5-Operadores&Expressoes/9_OperadoresExponenciacao/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+    # Podemos realizar o cálculo de potência com o símbolo **;
+    # Exemplo: 5 ** 2;
+    # Desta maneira teremos o resultado de 5 elevado a 2;
+
+
+    $a = 5;
+    $b = 2;
+
+    echo $a ** $b; //25
+
+
+
+
+
+
+?>
