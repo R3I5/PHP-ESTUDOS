0c2842fc629f5a3dc33db33f345714fa1a88afb2
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-07-13 20:57:46 -0300

    Operador de Concatenação

---
+++ Cap5-Operadores&Expressoes/10_OperadorConcatenacao/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+    # Em PHP podemos concatenar valores com . 
+    # Concatenar é o ato de juntar vários textos e/ou números em apenas uma string;
+    # Não há llimites de quantas expressões podem ser concatenadas;
+
+    echo "testando a concatenação1";
+    echo "<br>";
+    echo "testado " . "a " . "concatenação2";
+    echo "<br>";
+
+    $t = " testando";
+    $c = " concatenação3";
+    echo $t . "a" . $c;
+    echo "<br>";
+
+    $dia = 2;
+    $mês = "junho";
+    $ano = 2004;
+
+    echo "Minha data de nascimento é: " . $dia . " de " . $mês . " de " . $ano
+
+
+
+
+
+
+
+
+?>
