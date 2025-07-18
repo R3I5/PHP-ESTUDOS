5776d91acf6a9d890da6d7a42afcc8d77b1eccbe
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-07-18 13:10:33 -0300

    Exercicio Concatenação

---
+++ Cap5-Operadores&Expressoes/11_Exercicio14/index.php
@@ -0,0 +1,17 @@
+<?php   
+
+    # Crie um arquivo PHP;
+    # Crie uma variavel saudação, nome e outra de  sobrenome;
+    # imprima com echo a concatenação dde saudação, nome e sobrenome;
+
+    $saudacao = "Olá! ";
+    $nome = "João ";
+    $sobrenome = "Reis";
+    $frase = $saudacao . $nome . $sobrenome;
+    echo "$frase"
+
+
+
+
+
+?>
