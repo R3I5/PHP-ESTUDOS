8bdd562df86dd2641288a10490470e7f3643de56
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-07-18 13:23:05 -0300

    Operadores de Incremento e decremento

---
+++ Cap5-Operadores&Expressoes/12_AutoMenosAutoMais/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+    # Auto incremento e auto decremento
+    # Podemos incrementar um valor ou decrementar com os operadores: ++ e --;
+    # Exemplo: $n++ ou |$x--
+    # Onde nn e x são variaveis, e terão seus valores alterados com +1 e -1;
+    # Estes operadores são muito utilizados em estruturas de repetição;
+
+
+    $x = 0;
+    $y = 0;
+
+    $x++;
+    $y = $y + 1;
+
+    echo "x: " . $x . "<br>";
+    echo "y: " . $y . "<br>";
+
+    $n = 10;
+    $m = 10;
+
+    $n--;
+    $m = $m - 1;
+
+    echo "n: " . $n . "<br>";
+    echo "m: " . $m . "<br>";
+
+
+?>
