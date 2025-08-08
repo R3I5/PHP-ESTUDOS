8cd027f1b04ffae5510a318f0a00efe765d1c406
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-08-08 11:51:16 -0300

    Exercicio 15 Operador de igualdade

---
+++ Cap5-Operadores&Expressoes/14_Exercicio15/index.php
@@ -0,0 +1,27 @@
+<?php 
+
+    # Crie uma operação que retorne falso com igualdade;
+    # Crie uma operação que retorne verdadeiro com igualdade;
+
+    $a = 20;
+    $b = 15;
+    $c = 20;
+
+    if ($a == $b){
+        echo "Afirmação correta! A e B ";
+    }
+
+    if ($a == $c){
+        echo "Afirmação correta! A e C";
+    }
+
+
+
+
+
+
+
+
+
+
+?>
