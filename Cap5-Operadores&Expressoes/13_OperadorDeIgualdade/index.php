42d7187c575eefbbbd56a30d6cb9e458167b5cea
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-08-08 11:42:21 -0300

    Aula 13 Operador de Igualdade

---
+++ Cap5-Operadores&Expressoes/13_OperadorDeIgualdade/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+    # Com o operador de igualdade verificamos se um valor é igual ao outro;
+    # O simbolo é ==
+    # Exemplo: 5 == 4 # false
+    # Exemplo: 3 == 3 # true
+
+    if(3 == 4){
+        echo "Comparação verdadeira <br>";
+    }
+    else{
+        echo "Comparação 1 falsa <br>";
+    }
+
+    $meuNome = "João Victor Dos Reis";
+    $nomeDoSistema = "João Victor Da Silva";
+
+    if($meuNome == $nomeDoSistema){
+        echo "Logado! <br>";
+    }
+    else{
+        echo "Informações erradas! <br>";
+    }
+
+
+
+
+
+
+?>
