0301dad38498698fdbe6c1fd15c67dd9bd1df293
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-08-15 10:18:53 -0300

    Operador idêntico

---
+++ Cap5-Operadores&Expressoes/15_OperadorIdêntico/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+    # Com o operador idêntico a verificamos se um valor é igual ao outro, avaliando o seu tipo também;
+    # O simbolo é: ===
+    # Exemplo: 5 === 5 #true
+    # Exemplo: 3 === "3" # false
+
+    
+
+    
+
+
+
+?>
