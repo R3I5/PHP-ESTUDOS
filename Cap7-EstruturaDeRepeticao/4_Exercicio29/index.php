1fa6196d43dda526557ea18cf862edcbc62c6130
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-08-19 09:17:38 -0300

    Exercicio 29

---
+++ Cap7-EstruturaDeRepeticao/4_Exercicio29/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    # Crie um array com valores inteiros de 10 a 100, com incremento de 10;
+    # Aplique um loop neste array; Quando entrar os valores 30 e 40, pule para a próxima execução;
+
+    $arr = [10,20,30,40,50,60,70,80,90,100];
+    $i = 0;
+    
+
+
+?>
