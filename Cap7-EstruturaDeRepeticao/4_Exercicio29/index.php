a374922227299a24e1f212081bcfc8cae9cc3dc3
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-08-21 08:43:06 -0300

    Exercicio 29 Array/While

--- Cap7-EstruturaDeRepeticao/4_Exercicio29/index.php
+++ Cap7-EstruturaDeRepeticao/4_Exercicio29/index.php
@@ -5,6 +5,15 @@
 
     $arr = [10,20,30,40,50,60,70,80,90,100];
     $i = 0;
+
+    while($i < count($arr)) {
+        if ($arr[$i] == 30 || $arr[$i] == 40){
+            $i++;
+            continue;
+        }
+    echo "Elemento $arr[$i] <br>";
+    $i++;
+    }
     
 
 
